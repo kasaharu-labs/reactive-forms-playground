e26819d8d07ee9a6839896f24eb655363879469f
Wataru.Kasahara <w.kasahara@emotion-tech.co.jp>
2023-11-05 15:04:21 +0900

    feat: タスクボード追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet, NgFor, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-forms-playground';
+
+  todoForm = new FormGroup({
+    list: new FormArray([new FormControl('')]),
+  });
+
+  ngOnInit(): void {
+    this.todoForm.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  addNewTaskForm() {
+    this.todoForm.controls.list.push(new FormControl(''));
+  }
+
+  onPaste(event: ClipboardEvent, index: number) {
+    const inputData = event.clipboardData?.getData('text');
+    const [first, ...rest] = inputData?.split('\n') ?? [];
+
+    setTimeout(() => {
+      this.todoForm.controls.list.controls[index].setValue(first);
+    }, 0);
+
+    rest.forEach((item) => {
+      this.todoForm.controls.list.push(new FormControl(item));
+    });
+  }
 }
