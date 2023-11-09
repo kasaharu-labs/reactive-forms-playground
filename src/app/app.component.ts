4fd64483da50697707bef2adee1a3290dfe9f9be
Wataru.Kasahara <kasahara1019@gmail.com>
2023-11-10 08:17:23 +0900

    feat: フォームの内容をコピーできるようにした

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,14 @@
-import { NgFor } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import {
-  FormArray,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, ReactiveFormsModule],
+  imports: [RouterOutlet, TaskFormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'reactive-forms-playground';
-
-  todoForm = new FormGroup({
-    list: new FormArray([new FormControl('')]),
-  });
-
-  ngOnInit(): void {
-    this.todoForm.valueChanges.subscribe((value) => {
-      console.log(value);
-    });
-  }
-
-  addNewTaskForm() {
-    this.todoForm.controls.list.push(new FormControl(''));
-  }
-
-  onPaste(event: ClipboardEvent, index: number) {
-    const inputData = event.clipboardData?.getData('text');
-    const [first, ...rest] = inputData?.split('\n') ?? [];
-
-    setTimeout(() => {
-      this.todoForm.controls.list.controls[index].setValue(first);
-    }, 0);
-
-    rest.forEach((item) => {
-      this.todoForm.controls.list.push(new FormControl(item));
-    });
-  }
 }
--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './task-form.component.html',
-  styleUrl: './task-form.component.scss'
+  styleUrl: './task-form.component.scss',
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit {
+  todoForm = new FormGroup({
+    list: new FormArray([new FormControl('')]),
+  });
 
+  ngOnInit(): void {
+    this.todoForm.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  addNewTaskForm() {
+    this.todoForm.controls.list.push(new FormControl(''));
+  }
+
+  onPaste(event: ClipboardEvent, index: number) {
+    const inputData = event.clipboardData?.getData('text');
+    const [first, ...rest] = inputData?.split('\n') ?? [];
+
+    setTimeout(() => {
+      this.todoForm.controls.list.controls[index].setValue(first);
+    }, 0);
+
+    rest.forEach((item) => {
+      this.todoForm.controls.list.push(new FormControl(item));
+    });
+  }
+
+  copyTask(): void {
+    const { list } = this.todoForm.getRawValue();
+    if (list.length === 0) return;
+
+    const listString = list.join('\n');
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(listString);
+    }
+  }
 }
